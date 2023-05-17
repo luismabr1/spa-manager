42db65d657d4e0285ce418705083c05b1fd46aed
Luis Manuel Brito <lsbrt4@gmail.com>
2023-05-17 10:12:29 -0400

    creo que agarro el cambio, ya no sale el error de cors pero debo cambiar el localhost por el dominio

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
+            : 'https://spa-manager.vercel.app/api' // production api
     }
 }
 
