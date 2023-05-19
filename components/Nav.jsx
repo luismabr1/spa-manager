bf7b200fcf320b95a558e750e308f27531847b94
Luis Manuel Brito <lsbrt4@gmail.com>
2023-05-19 08:29:54 -0400

    quito lo de prueba del nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -26,46 +26,40 @@ function Nav() {
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="dropdown" id="navbarSupportedContent">
-  <ul className="navbar-nav mr-auto">
+                  <ul className="navbar-nav mr-auto">
 
-  <             NavLink href="/" exact className="nav-item nav-link">Home</NavLink>
-                <NavLink href="/users" className="nav-item nav-link">Users</NavLink>
-                <NavLink href="/clientes" className="nav-item nav-link">Clientes</NavLink>
+                  <             NavLink href="/" exact className="nav-item nav-link">Home</NavLink>
+                                <NavLink href="/users" className="nav-item nav-link">Users</NavLink>
+                                <NavLink href="/clientes" className="nav-item nav-link">Clientes</NavLink>
+                                <button onClick={userService.logout} className="btn btn-link nav-item nav-link">Logout</button>
 
 
-    <li className="nav-item dropdown">
-      <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown
-      </a>
-      <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-        <a className="dropdown-item" href="#">Action</a>
-        <a className="dropdown-item" href="#">Another action</a>
-        <div className="dropdown-divider"></div>
-        <a className="dropdown-item" href="#">Something else here</a>
-      </div>
-    </li> 
+                    {/* <li className="nav-item dropdown">
+                      <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                      </a>
+                      <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Action</a>
+                        <a className="dropdown-item" href="#">Another action</a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="#">Something else here</a>
+                      </div>
+                    </li>  */}
 
-{/* <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
-  </button>
-  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a className="dropdown-item" href="#">Action</a>
-    <a className="dropdown-item" href="#">Another action</a>
-    <a className="dropdown-item" href="#">Something else here</a>
-  </div> */}
-  </ul>
+                {/* <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown button
+                  </button>
+                  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                  </div> */}
+                  </ul>
 {/*   <form className="form-inline my-2 my-lg-0">
     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
   </form> */}
-</div>
-
-            <div className="navbar-nav">
-                <NavLink href="/" exact className="nav-item nav-link">Home</NavLink>
-                <NavLink href="/users" className="nav-item nav-link">Users</NavLink>
-                <NavLink href="/clientes" className="nav-item nav-link">Clientes</NavLink>
-                <button onClick={userService.logout} className="btn btn-link nav-item nav-link">Logout</button>
-            </div>
+          </div>
         </nav>
 
 
